686df926aa95423468ec18992cc50b2f895acec2
vasumukkapati <141704822+vasumukkapati@users.noreply.github.com>
2026-04-07 11:08:14 +0530

    Add files via upload

---
+++ Week-6/Week-6_Day-1_Hands_on/Week-6_Day-4_Hands_on/P2_Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication6.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Sample data (no database)
+        private List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 50000 },
+            new Product { Id = 2, Name = "Mobile", Price = 20000 },
+            new Product { Id = 3, Name = "Headphones", Price = 2000 }
+        };
+
+        // INDEX → List of products
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        // DETAILS → Single product
+        public IActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+    }
+}
---
+++ Week-6/Week-6_Day-1_Hands_on/Week-6_Day-4_Hands_on/P2_Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication6.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
